cd632922d9e8ad6e019034f6f94ee92b3630555d
mfbradley <mbradleystylist@gmail.com>
2019-04-10 10:44:35 -0400

    updating file paths

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,6 +9,6 @@ module.exports = function(app) {
     });
 
     app.get("/survey", function(request, response) {
-        response.sendFile(path.join(__dirname, "../public/survey.html"));
+        response.sendFile(path.join(__dirname, "survey.html"));
     });
 };
